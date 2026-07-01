ed435f8205bdedec6ac0540039399751c97cb56e
Lucas Giordano <lucasgiordano6789@gmail.com>
2026-06-30 22:29:48 -0300

    primer commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useEffect, useRef } from 'react'; // <--- ESTA LÍNEA ES LA QUE FALTA
+import { inicializarEntorno } from './main';
+
+export default function App() {
+  const canvasRef = useRef(null); // Ahora sí funcionará sin dar error
+
+  useEffect(() => {
+    const limpiarEntorno = inicializarEntorno(canvasRef.current);
+    return () => {
+      if (limpiarEntorno) limpiarEntorno();
+    };
+  }, []);
+
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+      <canvas ref={canvasRef} />
+    </div>
+  );
+}
+
